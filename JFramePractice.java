805e2152dc8c9ab643df0e240db71fb1b897ede7
AellnChien <64013614+lon040680@users.noreply.github.com>
2021-01-07 10:35:51 +0800

    Update and rename JFramePractice to JFramePractice.java

--- JFramePractice.java
+++ JFramePractice.java
@@ -84,8 +84,9 @@ public class JFramePractice extends javax.swing.JFrame {
         });
         timer.setRepeats(true);
         timer.start();
-    }                                        
-
+    }
+    
+    // 主類別
     public static void main(String args[]) {
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
